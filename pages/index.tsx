6ffd01efeb55f964dfe7bffc5dd9579196a41821
Derek Wiers <dtwiers08@gmail.com>
2021-08-21 00:29:21 -0500

    add react-leaflet; pnpm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,36 @@
+import React, { useEffect, useState } from 'react'
 import Page from '@/components/page'
 
-const Index = () => (
-	<Page>
-		<section className='mt-20'>
-			<h2 className='text-xl font-semibold text-gray-800 dark:text-gray-200'>
-				We grow a lot of rice.
-			</h2>
+const Index = () => {
+	const [location, setLocation] = useState<GeolocationPosition | null>(null)
+	useEffect(() => {
+	if (typeof window !== 'undefined' && 'geolocation' in navigator) {
+		const interval = setInterval(() => navigator.geolocation.getCurrentPosition(setLocation), 1000);
+		return () => clearInterval(interval);
+	}
+}, []);
+	return (
+		<Page>
+			<section className='mt-20'>
+				<h2 className='text-xl font-semibold text-gray-800 dark:text-gray-200'>
+					We grow a lot of rice.
+				</h2>
 
-			<p className='mt-2 text-gray-600 dark:text-gray-400'>
-				You love rice, and so does the rest of the world. In the crop year
-				2008/2009, the milled rice production volume amounted to over{' '}
-				<span className='font-medium text-gray-900 dark:text-gray-50'>
-					448 million tons
-				</span>{' '}
-				worldwide.
-			</p>
-		</section>
-	</Page>
-)
+				<p className='mt-2 text-gray-600 dark:text-gray-400'>
+					latitude: {location?.coords?.latitude}
+				</p>
+				<p className='mt-2 text-gray-600 dark:text-gray-400'>
+					longitude: {location?.coords?.longitude}
+				</p>
+				<p className='mt-2 text-gray-600 dark:text-gray-400'>
+					altitude: {location?.coords?.altitude}
+				</p>
+				<p className='mt-2 text-gray-600 dark:text-gray-400'>
+					speed: {location?.coords?.speed}
+				</p>
+			</section>
+		</Page>
+	)
+}
 
 export default Index
