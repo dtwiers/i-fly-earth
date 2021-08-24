0b412c312f0bd6ba51d182650ba29dd40d08b1ab
Derek Wiers <dtwiers08@gmail.com>
2021-08-24 00:25:48 -0500

    removed food-related themes; added more geo info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import Page from "@/components/page";
-import { useClientSide } from "@/src/hooks/use-client-side";
 import { Map, Marker } from "pigeon-maps";
 import React, { useEffect, useState } from "react";
+import { Ord } from "fp-ts/number";
+import { clamp } from "fp-ts/Ord";
 
 const Index = () => {
   const [location, setLocation] = useState<GeolocationPosition | null>(null);
-  const isClientSide = useClientSide();
-  const [zoom, setZoom] = useState<number>(10)
-  const incZoom = () => setZoom(z => z + 1);
-  const decZoom = () => setZoom(z => z - 1);
+  const [zoom, setZoom] = useState<number>(10);
+  const incZoom = () => setZoom((z) => clamp(Ord)(1, 19)(z + 1));
+  const decZoom = () => setZoom((z) => clamp(Ord)(1, 19)(z - 1));
   useEffect(() => {
     if ("geolocation" in navigator) {
       const interval = setInterval(
@@ -22,8 +22,23 @@ const Index = () => {
   return (
     <Page>
       <section className="mt-20">
-        <button type="button" onClick={incZoom}>+</button>
-        <button type="button" onClick={decZoom}>-</button>
+        <div className="flex justify-items-start items-center">
+          <button
+            type="button"
+            onClick={incZoom}
+            className="w-6 h-8 bg-gray-700 hover:bg-gray-800"
+          >
+            +
+          </button>
+          <button
+            type="button"
+            onClick={decZoom}
+            className="w-6 h-8 bg-gray-700 hover:bg-gray-800"
+          >
+            -
+          </button>
+          <p className="h-8 ml-3 flex items-center">Zoom Level: {zoom}</p>
+        </div>
         <Map
           height={300}
           center={[
@@ -33,7 +48,9 @@ const Index = () => {
           zoom={zoom}
         >
           <Marker
-            width={location?.coords.accuracy}
+            width={clamp(Ord)(4, 45)(
+              Math.sqrt(location?.coords.accuracy ?? 0 * 10)
+            )}
             anchor={[
               location?.coords.latitude ?? 0,
               location?.coords.longitude ?? 0,
@@ -50,7 +67,18 @@ const Index = () => {
           altitude: {location?.coords?.altitude}
         </p>
         <p className="mt-2 text-gray-600 dark:text-gray-400">
-          speed: {location?.coords?.speed}
+          speed: {location?.coords?.speed}m/s (
+          {(location?.coords?.speed ?? 0) * 2.2369} mph)
+        </p>
+        <p className="mt-2 text-gray-600 dark:text-gray-400">
+          heading: {location?.coords?.heading}
+        </p>
+        <p className="mt-2 text-gray-600 dark:text-gray-400">
+          altitude Accuracy: {location?.coords?.altitudeAccuracy}
+        </p>
+        <p className="mt-2 text-gray-600 dark:text-gray-400">
+          accuracy: {location?.coords?.accuracy} (
+          {clamp(Ord)(4, 45)(Math.sqrt(location?.coords.accuracy ?? 0 * 10))})
         </p>
       </section>
     </Page>
